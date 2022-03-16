59f2f1d257d22b7131531437596829534d2d0f83
AmmepalliSudarshan <96739628+AmmepalliSudarshan@users.noreply.github.com>
2022-03-16 11:20:44 +0530

    Add files via upload

---
+++ Problem_Statement_1.3/src/Book.java
@@ -0,0 +1,29 @@
+
+public class Book
+{
+    private String bookName;
+    private float bookPrice;
+    
+    
+     public void setBookName(String bookName)
+    {
+        this.bookName=bookName;
+    }
+    
+    public String getBookName()
+    {
+        return this.bookName;
+    }
+    
+    public void setBookPrice(float bookPrice)
+    {
+        this.bookPrice=bookPrice;
+    }
+    
+    public float getBookPrice()
+    {
+        return this.bookPrice;
+    }
+    
+    
+}
